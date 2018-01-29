543ddadf79ca48014f61963ed2ad546fc58bad2a
NewDimix <dimix93@mail.ru>
2018-01-29 13:59:41 +0600

    roadmap slider minor fixes

--- app/js/app.js
+++ app/js/app.js
@@ -208,7 +208,7 @@ $(window).on('load', function () {
 
 
   $(".js-roadmap").css("min-height", ($(".roadmap__dots").height() + 240) + "px");
-  alert($(".roadmap__dots").height() + 240);
+  alert($(".roadmap__dots").height() + 40);
 
   $(window).resize(function(){
     $(".js-roadmap").css("min-height", ($(".roadmap__dots").height() + 240) + "px");
